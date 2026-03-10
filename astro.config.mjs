d8ab0266fbedd5bf2e30fa0258e73b4590470584
josemorenourrutia <jose.ramirez@insbruguers.cat>
2026-03-10 14:22:52 +0100

    Traducción

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://josemorenourrutia.github.io/00-Documentacion-MicraCity',
-  // base: '/00-Documentacion-MicraCity/dist/', // 🔑 importante para GitHub Pages
+  base: '/00-Documentacion-MicraCity/dist/', // 🔑 importante para GitHub Pages
 
   build: {
     assets: 'assets' // 🔑 cambia _astro → assets
